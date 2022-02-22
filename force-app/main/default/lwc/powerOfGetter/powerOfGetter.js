f80df0b22c086853e03fe6075d8a237534bcd99b
pratikdka <pratik.khadka18@gmail.com>
2022-02-22 22:24:59 +0545

    newcommit

--- force-app/main/default/lwc/powerOfGetter/powerOfGetter.js
+++ force-app/main/default/lwc/powerOfGetter/powerOfGetter.js
@@ -1,8 +1,18 @@
 import { LightningElement } from 'lwc';
 
 export default class PowerOfGetter extends LightningElement {
-title = 'This is a getter Power'
+
+title = 'This is a getter Power';
+selectedBtn = 'OFF';
 get getTitle(){
-  return this.title.toUpperCase()
+return this.title.toUpperCase()
+}
+get boxStatus(){
+// return this.selectedBtn === 'ON' ? 'box green' : 'box red' ;
+return `box ${this.selectedBtn === 'ON' ? 'green':'red'}`
+}
+clickAction(event){
+console.log(event.currentTarget.innerText);
+this.selectedBtn = event.currentTarget.innerText;
 }
 }
